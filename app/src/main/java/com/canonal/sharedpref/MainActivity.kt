14265885a43fad94ca27dbccb1a9a6957efb3bbd
canonall <onalcan20@gmail.com>
2021-11-29 16:14:15 +0100

    EncryptedSharedPreferences added

--- app/src/main/java/com/canonal/sharedpref/MainActivity.kt
+++ app/src/main/java/com/canonal/sharedpref/MainActivity.kt
@@ -5,6 +5,9 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import androidx.security.crypto.MasterKeys
 import com.canonal.sharedpref.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,27 +25,59 @@ class MainActivity : AppCompatActivity() {
         val sharedPref = getSharedPreferences("Sample Shared Preferences", Context.MODE_PRIVATE)
         val pref = getPreferences(Context.MODE_PRIVATE)
 
+        //ENCRYPTED SHARED PREFERENCES
+        //reading and writing is same as normal shared preferences
+        //below code is for version 1.0.0 which supports API 23+
+//        val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+//        val mainKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+//        val encryptedSharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
+//            "Encrypted Shared Preferences Sample",
+//            mainKeyAlias,
+//            applicationContext,
+//            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+//            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+//        )
+
+        //For version 1.1.0-alpha03 which supports API 21+
+        val mainKey = MasterKey.Builder(applicationContext)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+        val encryptedSharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
+            applicationContext,
+            "Encrypted Shared Preferences Sample",
+            mainKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
         //read data using object
         val usernameSharedPref = sharedPref.getString("username", "default value")
         val usernamePref = pref.getString("username", "default value")
+        val usernameEncryptedSharedPref =
+            encryptedSharedPreferences.getString("username", "default value")
 
-        val checkBoxStateSharedPref = sharedPref.getBoolean("checkBoxState", false)
-        val checkBoxStatePref = pref.getBoolean("checkBoxState", false)
+        val cbStateSharedPref = sharedPref.getBoolean("cbState", false)
+        val cbStatePref = pref.getBoolean("cbState", false)
+        val cbStateEncryptedSharedPref = encryptedSharedPreferences.getBoolean("cbState", false)
 
 
         if (usernameSharedPref?.isEmpty() == false) {
             etUsername.setText(usernameSharedPref)
-            cbRememberMe.isChecked = checkBoxStateSharedPref
+            cbRememberMe.isChecked = cbStateSharedPref
         }
         if (usernamePref?.isEmpty() == false) {
             etUsername.setText(usernamePref)
-            cbRememberMe.isChecked = checkBoxStatePref
-
+            cbRememberMe.isChecked = cbStatePref
+        }
+        if (usernameEncryptedSharedPref?.isEmpty() == false) {
+            etUsername.setText(usernameEncryptedSharedPref)
+            cbRememberMe.isChecked = cbStateEncryptedSharedPref
         }
 
         //write to shared preferences
         val sharedPrefEditor = sharedPref.edit()
         val prefEditor = pref.edit()
+        val encryptedSharedPrefEditor = encryptedSharedPreferences.edit()
 
         btnLogin.setOnClickListener {
             if (cbRememberMe.isChecked) {
@@ -50,18 +85,20 @@ class MainActivity : AppCompatActivity() {
                 saveOrClearSharedPreferences(
                     sharedPrefEditor,
                     prefEditor,
+                    encryptedSharedPrefEditor,
                     etUsername.text.toString(),
                     true
                 )
-                applySharedPreferences(sharedPrefEditor, prefEditor)
+                applySharedPreferences(sharedPrefEditor, prefEditor, encryptedSharedPrefEditor)
             } else {
                 saveOrClearSharedPreferences(
                     sharedPrefEditor,
                     prefEditor,
+                    encryptedSharedPrefEditor,
                     "",
                     false
                 )
-                applySharedPreferences(sharedPrefEditor, prefEditor)
+                applySharedPreferences(sharedPrefEditor, prefEditor, encryptedSharedPrefEditor)
             }
         }
 
@@ -70,19 +107,23 @@ class MainActivity : AppCompatActivity() {
     private fun saveOrClearSharedPreferences(
         sharedPrefEditor: SharedPreferences.Editor,
         prefEditor: SharedPreferences.Editor,
+        encryptedSharedPrefEditor: SharedPreferences.Editor,
         username: String,
         cbState: Boolean
     ) {
         sharedPrefEditor.putString("username", username)
         prefEditor.putString("username", username)
+        encryptedSharedPrefEditor.putString("username", username)
 
-        sharedPrefEditor.putBoolean("checkBoxState", cbState)
-        prefEditor.putBoolean("checkBoxState", cbState)
+        sharedPrefEditor.putBoolean("cbState", cbState)
+        prefEditor.putBoolean("cbState", cbState)
+        encryptedSharedPrefEditor.putBoolean("cbState", cbState)
     }
 
     private fun applySharedPreferences(
         sharedPrefEditor: SharedPreferences.Editor,
         prefEditor: SharedPreferences.Editor,
+        encryptedSharedPrefEditor: SharedPreferences.Editor,
     ) {
         //writes data synchronously
         //sharedPrefEditor.commit()
@@ -91,5 +132,6 @@ class MainActivity : AppCompatActivity() {
         //writes data asynchronously
         sharedPrefEditor.apply()
         prefEditor.apply()
+        encryptedSharedPrefEditor.apply()
     }
 }
